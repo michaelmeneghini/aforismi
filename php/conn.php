22cfcee78fbe6b53c74ea9524611ebbb3752744a
Michael Meneghini <michael98menega@gmail.com>
2017-02-28 15:45:52 +0100

    Added conn.php and logout.php

--- php/conn.php
+++ php/conn.php
@@ -10,7 +10,6 @@
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $dbpassword);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo("connection successful");
     }
     catch(PDOException $e){
         echo "Connection failed: " . $e->getMessage();
