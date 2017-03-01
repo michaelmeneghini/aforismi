3abb7f12019d5aa9ab4b2c52930db836cd87664c
Michael Meneghini <michael98menega@gmail.com>
2017-03-01 14:09:06 +0100

    implemented registration core

--- php/register_manager.php
+++ php/register_manager.php
@@ -1,4 +1,38 @@
 <?php
+    include 'conn.php';
+
+    //input data management
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = test_input($_POST["email"]);
+        $firstname = test_input($_POST["first_namde"]);
+        $lastname = test_input($_POST["last_name"]);
+        $password = test_input(encryptPassword($_POST["password"]));
+    }
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    function encryptPassword($password){
+        $result = sha1($password,false);
+        return $result;
+    }
+
+    //registering user
+    try{
+        $sql = "INSERT INTO users (email, first_name, last_name, password)
+                    VALUES ('$email', '$firstname', '$lastname', '$password')";
+        $conn->exec($sql);
+        echo("User registered successfully!"."<br>");
+    }
+    catch(PDOException $e){
+        echo("Couldn't register properly! .<br>. $e->getMessage()");
+    }
+
 
 
 ?>
